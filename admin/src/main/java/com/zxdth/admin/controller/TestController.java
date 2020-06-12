2b53297acd3c8c6d5d88358e25dc722b9632df84
admin <zhangth1013@163.com>
2020-06-12 16:07:14 +0800

    Initial commit

---
+++ admin/src/main/java/com/zxdth/admin/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.zxdth.admin.controller;
+
+
+import java.text.SimpleDateFormat;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zxdth.utils.helper.CommonMethod;
+
+@RestController
+@RequestMapping("/Test")
+public class TestController {
+
+    @GetMapping("/time")
+    public String getTime() {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return df.format( CommonMethod.getTime());
+    }
+}
---
+++ utils/src/main/java/com/zxdth/utils/helper/CommonMethod.java
@@ -0,0 +1,15 @@
+package com.zxdth.utils.helper;
+
+import java.util.Date;
+
+public class CommonMethod {
+
+    /**
+     * 获取当前日期
+     * @return
+     */
+    public static Date getTime(){
+        Date data = new Date();
+        return data;
+    }
+}
